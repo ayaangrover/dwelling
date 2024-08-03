e6b284b6e848a13bb38f7780f26aca7d849054f7
Ayaan Grover <ayaangrover@gmail.com>
2024-08-03 15:03:58 -0700

    fix dev team and test one week free app running

--- Dwelling/Dwelling/WebPageView.swift
+++ Dwelling/Dwelling/WebPageView.swift
@@ -4,7 +4,6 @@ struct WebPageView: View {
     var body: some View {
         NavigationView {
             WebView(url: URL(string: "https://msbell.harker.xyz")!)
-//                .navigationBarTitle("Web Page", displayMode: .inline)
         }
     }
 }
